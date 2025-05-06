b66ab6c251c6899a70d7a927f78129f5a3c72f57
LBarrys <lbarrys303@gmail.com>
2025-05-07 01:07:26 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -37,7 +37,7 @@ PS1='\[\033[1;32m\]\u\[\033[0;37m\]@\[\033[1;32m\]\h\[\033[0;37m\]:\W '
 #aliases
 alias ls='ls -a --color=auto'
 alias grep='grep --color=auto'
-alias autoremove='pacman -Qdtq && sudo pacman -Rs $(pacman -Qdtq)'
+alias autoremove='sudo pacman -Rns $(pacman -Qdtq)'
 alias install='sudo pacman -S'
 alias remove='sudo pacman -Rns'
 alias search='pacman -Ss'
