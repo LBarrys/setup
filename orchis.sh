46b535abc57a8cf77b8c5e32417fcde07ccf3cce
LBarrys <lbarrys303@gmail.com>
2025-06-22 17:40:53 +0300

    Create orchis.sh

---
+++ orchis.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cd
+git clone https://github.com/vinceliuice/Orchis-theme.git
+cd Orchis-theme
+./install.sh --theme green --color dark --size standard --icon simple --libadwaita --tweaks solid compact dock
+sudo cp -r ~/.themes/* /usr/share/themes
+
+sudo flatpak override --filesystem=xdg-config/gtk-3.0 && sudo flatpak override --filesystem=xdg-config/gtk-4.0
