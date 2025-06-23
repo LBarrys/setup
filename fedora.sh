9f2ea7bec8d9218a519786b5f8fcda9def384e22
LBarrys <lbarrys303@gmail.com>
2025-06-24 00:21:28 +0300

    Update fedora.sh

--- fedora.sh
+++ fedora.sh
@@ -13,8 +13,10 @@ defaultyes=True
 countme=false
 " >> /etc/dnf/dnf.conf
 
-# Install free and nonfree repositories
+# Install RPMfusion & terra repositories
 sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+dnf install --nogpgcheck --repofrompath 'terra,https://repos.fyralabs.com/terra$releasever' terra-release
+sudo dnf update
 
 # CachyOS kernel
 sudo setsebool -P domain_kernel_load_modules on
@@ -34,7 +36,7 @@ sudo sed -i "/^GRUB_CMDLINE_LINUX=/ s/\"$/ $GRUB_PARAM\"/" "$GRUB_FILE"
 sudo grub2-mkconfig -o "$OUTPUT_FILE"
 
 # Install RPMs
-sudo dnf install timeshift flatpak firefox thunderbird fastfetch vlc telegram-desktop steam wine winetricks protontricks mangohud papirus-icon-theme bat wget p7zip p7zip-plugins unzip unrar @virtualization
+sudo dnf install timeshift flatpak firefox thunderbird gnome-disk-utility fastfetch vlc telegram-desktop komikku steam bottles wine winetricks protontricks mangohud papirus-icon-theme bat wget p7zip p7zip-plugins unrar @virtualization
 sudo sed -i 's/#unix_sock_group = "libvirt"/unix_sock_group = "libvirt"/g' /etc/libvirt/libvirtd.conf
 sudo sed -i 's/#unix_sock_rw_perms = "0770"/unix_sock_rw_perms = "0770"/g' /etc/libvirt/libvirtd.conf
 sudo systemctl enable libvirtd
@@ -42,10 +44,10 @@ sudo usermod -aG libvirt "$(whoami)"
 
 # Install Flatpaks
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
-flatpak install flathub com.github.tchx84.Flatseal com.vysp3r.ProtonPlus io.github.Foldex.AdwSteamGtk io.github.radiolamp.mangojuice com.mattjakeman.ExtensionManager io.github.realmazharhussain.GdmSettings info.febvre.Komikku com.usebottles.bottles
+flatpak install flathub com.github.tchx84.Flatseal com.vysp3r.ProtonPlus io.github.Foldex.AdwSteamGtk io.github.radiolamp.mangojuice com.mattjakeman.ExtensionManager io.github.realmazharhussain.GdmSettings
 
 # GNOME
-# sudo dnf install gdm gnome-shell gnome-terminal gnome-tweaks nautilus gnome-terminal-nautilus gnome-disk-utility gnome-text-editor gnome-weather gnome-shell-extension-appindicator gnome-shell-extension-blur-my-shell gnome-shell-extension-dash-to-dock gnome-shell-extension-just-perfection gnome-shell-extension-user-theme transmission-gtk
+# sudo dnf install gdm gnome-shell gnome-terminal gnome-tweaks nautilus gnome-terminal-nautilus gnome-text-editor gnome-weather gnome-shell-extension-appindicator gnome-shell-extension-blur-my-shell gnome-shell-extension-dash-to-dock gnome-shell-extension-just-perfection gnome-shell-extension-user-theme transmission-gtk
 
 # Plasma
 # sudo dnf install sddm-kcm sddm-breeze plasma-desktop plasma-nm plasma-pa kscreen breeze-gtk kde-gtk-config xed nemo-fileroller alacritty transmission-qt breeze-cursor-theme
@@ -68,8 +70,7 @@ sudo systemctl start warp-svc.service
 warp-cli registration new
 
 # Install fonts
-sudo dnf copr enable aquacash5/nerd-fonts
-sudo dnf install google-roboto-fonts google-noto-fonts-all google-noto-fonts-all-static google-noto-fonts-all-vf google-noto-sans-cjk-fonts google-noto-sans-cjk-vf-fonts jet-brains-mono-nerd-fonts
+sudo dnf install google-roboto-fonts google-noto-fonts-all google-noto-fonts-all-static google-noto-fonts-all-vf google-noto-sans-cjk-fonts google-noto-sans-cjk-vf-fonts jetbrainsmono-nerd-fonts
 
 # Install MS fonts
 sudo dnf install curl cabextract xorg-x11-font-utils fontconfig
@@ -80,8 +81,9 @@ sudo fc-cache -fv
 sudo dnf remove zram* vim* gnome-tour gnome-color-manager malcontent-control virt-viewer
 sudo dnf autoremove
 
-# Enable graphical.target
+# Systemd
 sudo systemctl set-default graphical.target
+sudo systemctl disable NetworkManager-wait-online.service
 
 # My .bashrc
 echo "
