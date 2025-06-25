57e165993abf6fc810d4e64f2c6ca9d99d856234
LBarrys <201232108+LBarrys@users.noreply.github.com>
2025-06-25 22:36:56 +0300

    Update fedora.sh

--- fedora.sh
+++ fedora.sh
@@ -5,13 +5,13 @@ GRUB_FILE="/etc/default/grub"
 GRUB_PARAM="nvidia-drm.modeset=1"
 OUTPUT_FILE="/boot/grub2/grub.cfg"
 Nvidia="kmod-nvidia xorg-x11-drv-nvidia-cuda akmod-nvidia nvidia-vaapi-driver libva-utils"
-RPMs="timeshift grub-btrfs-timeshift cloudflare-warp flatseal firefox thunderbird gnome-disk-utility fastfetch vlc telegram-desktop mission-center steam bottles wine winetricks protontricks prismlauncher mangohud papirus-icon-theme bat wget p7zip p7zip-plugins unrar @virtualization"
+RPMs="timeshift grub-btrfs-timeshift cloudflare-warp flatseal firefox thunderbird gnome-disk-utility fastfetch vlc telegram-desktop mission-center steam steamtinkerlaunch bottles wine winetricks protontricks prismlauncher mangohud papirus-icon-theme bat wget p7zip p7zip-plugins unrar @virtualization"
 Flatpaks="com.vysp3r.ProtonPlus io.github.Foldex.AdwSteamGtk io.github.radiolamp.mangojuice info.febvre.Komikku"
 GNOME="gdm gnome-shell gnome-terminal gnome-tweaks nautilus gnome-terminal-nautilus gnome-text-editor com.mattjakeman.ExtensionManager io.github.realmazharhussain.GdmSettings gnome-weather gnome-shell-extension-appindicator gnome-shell-extension-blur-my-shell gnome-shell-extension-dash-to-dock gnome-shell-extension-just-perfection gnome-shell-extension-user-theme transmission-gtk breeze-cursor-theme"
 Plasma="sddm-kcm sddm-breeze plasma-desktop plasma-nm plasma-pa kscreen breeze-gtk kde-gtk-config xed nemo-fileroller alacritty transmission-qt"
 Niri="niri lightdm lightdm-settings lxappearance nemo nemo-fillroller xed transmission-gtk breeze-cursor-theme"
 Fonts="google-roboto-fonts google-noto-fonts-all google-noto-fonts-all-static google-noto-fonts-all-vf google-noto-sans-cjk-fonts google-noto-sans-cjk-vf-fonts ms-core-fonts jetbrainsmono-nerd-fonts"
-Trash="zram* vim* gnome-tour gnome-color-manager plasma-welcome malcontent-control virt-viewer"
+Trash="zram* vim* gnome-tour gnome-color-manager malcontent-control gnome-extensions-app gnome-remote-desktop gnome-bluetooth dosbox-staging speech-dispatcher speech-dispatcher-utils sane-backends-drivers-cameras sane-backends-drivers-scanners plasma-welcome virt-viewer"
 
 # Configure DNF
 echo "
@@ -31,6 +31,15 @@ sudo dnf copr enable bieszczaders/kernel-cachyos
 sudo dnf copr enable bieszczaders/kernel-cachyos-addons
 sudo dnf copr enable kylegospo/grub-btrfs
 sudo dnf update
+
+# GNOME
+# sudo dnf install $GNOME
+
+# Plasma
+# sudo dnf install $Plasma
+
+# Niri
+# sudo dnf install $Niri
 
 # Install CachyOS stuff
 sudo setsebool -P domain_kernel_load_modules on
@@ -55,15 +64,6 @@ sudo sed -i 's/#unix_sock_group = "libvirt"/unix_sock_group = "libvirt"/g' /etc/
 sudo sed -i 's/#unix_sock_rw_perms = "0770"/unix_sock_rw_perms = "0770"/g' /etc/libvirt/libvirtd.conf
 sudo systemctl enable libvirtd
 sudo usermod -aG libvirt "$(whoami)"
-
-# GNOME
-# sudo dnf install $GNOME
-
-# Plasma
-# sudo dnf install $Plasma
-
-# Niri
-# sudo dnf install $Niri
 
 # Multimedia
 sudo dnf swap ffmpeg-free ffmpeg --allowerasing
