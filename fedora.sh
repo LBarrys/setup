68d2f2a2664c9a81874b73a9630cba95eb362e2f
LBarrys <201232108+LBarrys@users.noreply.github.com>
2025-08-19 02:14:58 +0300

    Update fedora.sh

--- fedora.sh
+++ fedora.sh
@@ -5,10 +5,9 @@ GRUB_FILE="/etc/default/grub"
 GRUB_PARAM="nvidia-drm.modeset=1"
 OUTPUT_FILE="/boot/grub2/grub.cfg"
 Nvidia="kmod-nvidia xorg-x11-drv-nvidia-cuda akmod-nvidia nvidia-vaapi-driver libva-utils"
-RPMs="ghostty-nightly timeshift grub-btrfs-timeshift cloudflare-warp firefox thunderbird gnome-disk-utility fastfetch vlc telegram-desktop mission-center steam wine winetricks prismlauncher mangohud papirus-icon-theme bat wget p7zip p7zip-plugins unrar @virtualization"
-Flatpaks="com.vysp3r.ProtonPlus io.github.radiolamp.mangojuice info.febvre.Komikku com.mattjakeman.ExtensionManager io.github.realmazharhussain.GdmSettings io.github.Foldex.AdwSteamGtk com.usebottles.bottles com.github.tchx84.Flatseal"
-GNOME="gdm gnome-shell gnome-tweaks nautilus gnome-text-editor  gnome-weather gnome-shell-extension-appindicator gnome-shell-extension-blur-my-shell gnome-shell-extension-dash-to-dock gnome-shell-extension-just-perfection gnome-shell-extension-user-theme transmission-gtk breeze-cursor-theme"
-Fonts="google-roboto-fonts google-noto-fonts-all google-noto-fonts-all-static google-noto-fonts-all-vf google-noto-sans-cjk-fonts google-noto-sans-cjk-vf-fonts ms-core-fonts jetbrainsmono-nerd-fonts"
+RPMs="ghostty nautilus-open-any-terminal timeshift grub-btrfs-timeshift cloudflare-warp firefox thunderbird gnome-disk-utility fastfetch vlc Komikku telegram-desktop mission-center steam wine winetricks prismlauncher protonplus papirus-icon-theme bat wget p7zip p7zip-plugins unrar @virtualization"
+GNOME="gdm gdm-settings gnome-shell gnome-tweaks nautilus gnome-text-editor extension-manager gnome-weather gnome-shell-extension-appindicator gnome-shell-extension-blur-my-shell gnome-shell-extension-dash-to-dock gnome-shell-extension-just-perfection gnome-shell-extension-user-theme transmission-gtk breeze-cursor-theme"
+Fonts="google-roboto-fonts google-noto-fonts-all google-noto-sans-cjk-fonts ms-core-fonts jetbrainsmono-nerd-fonts"
 Trash="zram* vim* gnome-tour gnome-color-manager malcontent-control gnome-extensions-app gnome-remote-desktop gnome-bluetooth dosbox-staging speech-dispatcher speech-dispatcher-utils sane-backends-drivers-cameras sane-backends-drivers-scanners virt-viewer"
 
 # Configure DNF
@@ -16,24 +15,25 @@ sudo echo '
 fastestmirror=True
 max_parallel_downloads=5
 defaultyes=True
-countme=false
 ' >> /etc/dnf/dnf.conf
 
 # Install/Enable repositories
-sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
-sudo dnf install flatpak
-flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
-sudo dnf install --nogpgcheck --repofrompath 'terra,https://repos.fyralabs.com/terra$releasever' terra-release
-curl -fsSl https://pkg.cloudflareclient.com/cloudflare-warp-ascii.repo | sudo tee /etc/yum.repos.d/cloudflare-warp.repo
-sudo dnf copr enable bieszczaders/kernel-cachyos
-sudo dnf copr enable bieszczaders/kernel-cachyos-addons
-sudo dnf copr enable kylegospo/grub-btrfs
+sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y
+sudo dnf install --nogpgcheck --repofrompath 'terra,https://repos.fyralabs.com/terra$releasever' terra-release -y
+curl -fsSl https://pkg.cloudflareclient.com/cloudflare-warp-ascii.repo | sudo tee /etc/yum.repos.d/cloudflare-warp.repo -y
+sudo dnf copr enable bieszczaders/kernel-cachyos -y
+sudo dnf copr enable bieszczaders/kernel-cachyos-addons -y
+sudo dnf copr enable kylegospo/grub-btrfs -y
+sudo dnf copr enable wehagy/protonplus -y
+sudo dnf copr enable umutd3401/extension-manager -y
+sudo dnf copr enable reanimator/gdm-settings -y
+sudo dnf copr enable monkeygold/nautilus-open-any-terminal -y
 sudo dnf update
 
 # GNOME
 sudo dnf install $GNOME
 
-# Install CachyOS stuff
+# CachyOS stuff
 sudo setsebool -P domain_kernel_load_modules on
 sudo dnf install kernel-cachyos kernel-cachyos-devel-matched scx-scheds
 sudo systemctl enable --now scx.service
@@ -41,7 +41,7 @@ sudo dnf install libcap-ng libcap-ng-devel procps-ng procps-ng-devel
 sudo dnf install uksmd
 sudo ksmctl -e
 
-# Install nvidia proprietary drivers
+# NVIDIA proprietary drivers
 sudo dnf install $Nvidia
 
 # Configure GRUB for nvidia drivers
@@ -51,7 +51,6 @@ sudo grub2-mkconfig -o "$OUTPUT_FILE"
 
 # RPMs & flatpaks & other stuff
 sudo dnf install $RPMs
-flatpak install flathub $Flatpaks
 sudo sed -i 's/#unix_sock_group = "libvirt"/unix_sock_group = "libvirt"/g' /etc/libvirt/libvirtd.conf
 sudo sed -i 's/#unix_sock_rw_perms = "0770"/unix_sock_rw_perms = "0770"/g' /etc/libvirt/libvirtd.conf
 sudo systemctl enable libvirtd
@@ -68,7 +67,7 @@ sudo dnf install --setopt=install_weak_deps=false gstreamer1-plugins-{bad-\*,goo
 sudo dnf install flexiblas-openblas-serial
 sudo dnf install lame\* --exclude=lame-devel
 
-# Install fonts
+# Fonts
 sudo dnf install $Fonts
 
 # Remove Firewalld's Default Rules
@@ -85,6 +84,7 @@ sudo dnf autoremove
 
 # Orchis theme
 cd
+sudo dnf in gnome-themes-extra gtk-murrine-engine -y
 git clone https://github.com/vinceliuice/Orchis-theme.git
 cd Orchis-theme
 ./install.sh --theme green --color dark --size standard --icon fedora --libadwaita --tweaks solid compact dock
