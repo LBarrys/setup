221da359ff83d129f57f0e90b58b470ca4dce902
LBarrys <201232108+LBarrys@users.noreply.github.com>
2025-08-18 18:31:09 +0300

    Update fedora.sh

--- fedora.sh
+++ fedora.sh
@@ -5,19 +5,19 @@ GRUB_FILE="/etc/default/grub"
 GRUB_PARAM="nvidia-drm.modeset=1"
 OUTPUT_FILE="/boot/grub2/grub.cfg"
 Nvidia="kmod-nvidia xorg-x11-drv-nvidia-cuda akmod-nvidia nvidia-vaapi-driver libva-utils"
-RPMs="alacritty timeshift grub-btrfs-timeshift cloudflare-warp flatseal firefox thunderbird gnome-disk-utility fastfetch vlc telegram-desktop mission-center steam steamtinkerlaunch bottles wine winetricks protontricks prismlauncher mangohud papirus-icon-theme bat wget p7zip p7zip-plugins unrar @virtualization"
-Flatpaks="com.vysp3r.ProtonPlus io.github.radiolamp.mangojuice info.febvre.Komikku"
-GNOME="gdm gnome-shell gnome-tweaks nautilus gnome-text-editor com.mattjakeman.ExtensionManager io.github.realmazharhussain.GdmSettings io.github.Foldex.AdwSteamGtk gnome-weather gnome-shell-extension-appindicator gnome-shell-extension-blur-my-shell gnome-shell-extension-dash-to-dock gnome-shell-extension-just-perfection gnome-shell-extension-user-theme transmission-gtk breeze-cursor-theme"
-Plasma="sddm-kcm sddm-breeze plasma-desktop plasma-nm plasma-pa kscreen breeze-gtk kde-gtk-config xed nemo-fileroller alacritty transmission-qt"
-Niri="niri lightdm lightdm-settings lxappearance nemo nemo-fillroller xed transmission-gtk breeze-cursor-theme"
+RPMs="ghostty-nightly timeshift grub-btrfs-timeshift cloudflare-warp firefox thunderbird gnome-disk-utility fastfetch vlc telegram-desktop mission-center steam wine winetricks prismlauncher mangohud papirus-icon-theme bat wget p7zip p7zip-plugins unrar @virtualization"
+Flatpaks="com.vysp3r.ProtonPlus io.github.radiolamp.mangojuice info.febvre.Komikku com.mattjakeman.ExtensionManager io.github.realmazharhussain.GdmSettings io.github.Foldex.AdwSteamGtk com.usebottles.bottles com.github.tchx84.Flatseal"
+GNOME="gdm gnome-shell gnome-tweaks nautilus gnome-text-editor  gnome-weather gnome-shell-extension-appindicator gnome-shell-extension-blur-my-shell gnome-shell-extension-dash-to-dock gnome-shell-extension-just-perfection gnome-shell-extension-user-theme transmission-gtk breeze-cursor-theme"
 Fonts="google-roboto-fonts google-noto-fonts-all google-noto-fonts-all-static google-noto-fonts-all-vf google-noto-sans-cjk-fonts google-noto-sans-cjk-vf-fonts ms-core-fonts jetbrainsmono-nerd-fonts"
-Trash="zram* vim* gnome-tour gnome-color-manager malcontent-control gnome-extensions-app gnome-remote-desktop gnome-bluetooth dosbox-staging speech-dispatcher speech-dispatcher-utils sane-backends-drivers-cameras sane-backends-drivers-scanners plasma-welcome virt-viewer"
+Trash="zram* vim* gnome-tour gnome-color-manager malcontent-control gnome-extensions-app gnome-remote-desktop gnome-bluetooth dosbox-staging speech-dispatcher speech-dispatcher-utils sane-backends-drivers-cameras sane-backends-drivers-scanners virt-viewer"
 
 # Configure DNF
-sudo echo "fastestmirror=True
+sudo echo '
+fastestmirror=True
 max_parallel_downloads=5
 defaultyes=True
-countme=false" >> /etc/dnf/dnf.conf
+countme=false
+' >> /etc/dnf/dnf.conf
 
 # Install/Enable repositories
 sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
@@ -31,13 +31,7 @@ sudo dnf copr enable kylegospo/grub-btrfs
 sudo dnf update
 
 # GNOME
-# sudo dnf install $GNOME
-
-# Plasma
-# sudo dnf install $Plasma
-
-# Niri
-# sudo dnf install $Niri
+sudo dnf install $GNOME
 
 # Install CachyOS stuff
 sudo setsebool -P domain_kernel_load_modules on
@@ -55,13 +49,17 @@ sudo cp "$GRUB_FILE" "${GRUB_FILE}.bak"
 sudo sed -i "/^GRUB_CMDLINE_LINUX=/ s/\"$/ $GRUB_PARAM\"/" "$GRUB_FILE"
 sudo grub2-mkconfig -o "$OUTPUT_FILE"
 
-# Install RPMs & flatpaks
+# RPMs & flatpaks & other stuff
 sudo dnf install $RPMs
 flatpak install flathub $Flatpaks
 sudo sed -i 's/#unix_sock_group = "libvirt"/unix_sock_group = "libvirt"/g' /etc/libvirt/libvirtd.conf
 sudo sed -i 's/#unix_sock_rw_perms = "0770"/unix_sock_rw_perms = "0770"/g' /etc/libvirt/libvirtd.conf
 sudo systemctl enable libvirtd
 sudo usermod -aG libvirt "$(whoami)"
+sudo systemctl disable NetworkManager-wait-online.service
+sudo systemctl enable --now grub-btrfs.path
+sudo systemctl enable warp-svc.service
+sudo systemctl set-default graphical.target
 
 # Multimedia
 sudo dnf swap ffmpeg-free ffmpeg --allowerasing
@@ -84,12 +82,6 @@ sudo firewall-cmd --reload
 # Remove unnecessary packages
 sudo dnf remove $Trash
 sudo dnf autoremove
-
-# Systemd services
-sudo systemctl set-default graphical.target
-sudo systemctl disable NetworkManager-wait-online.service
-sudo systemctl enable --now grub-btrfs.path
-sudo systemctl enable warp-svc.service
 
 # My configs
 mv ~/setup/wallpapers ~/.config
