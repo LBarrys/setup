823d88b9da09d613c24584473c64ebf50b81c717
LBarrys <201232108+LBarrys@users.noreply.github.com>
2025-08-31 12:32:59 +0300

    Update arch.sh

--- arch.sh
+++ arch.sh
@@ -7,9 +7,14 @@ cd yay-bin
 makepkg -si
 
 # GNOME
-GNOME="gdm gnome-shell gnome-tweaks gnome-control-center nautilus gnome-text-editor gdm-settings extension-manager gnome-weather transmission-gtk extra/breeze"
-yay -S $GNOME
-sudo systemctl enable gdm
+GNOME="gdm gnome-shell gnome-tweaks gnome-control-center nautilus gnome-text-editor gdm-settings extension-manager gnome-weather"
+# yay -S $GNOME
+# sudo systemctl enable gdm
+
+# Sway
+SWAY="lightdm-gtk-greeter lightdm-gtk-greeter-settings sway swaybg sway-nvivida autotiling waybar wofi nemo-fileroller xed polkit-gnome xdg-user-dirs xdg-user-dirs-gtk"
+# yay -S $SWAY
+# sudo systemctl enable lightdm
 
 # Install nvidia proprietary drivers
 Nvidia="nvidia-dkms nvidia-utils lib32-nvidia-utils opencl-nvidia lib32-opencl-nvidia linux-headers"
@@ -33,9 +38,9 @@ sudo sed -i "s/GRUB_CMDLINE_LINUX=\"\"/GRUB_CMDLINE_LINUX=\"loglevel=3 quiet nvi
 sudo grub-mkconfig -o /boot/grub/grub.cfg
 
 # Install RPMs & flatpaks
-PACMANs="ghostty timeshift grub-btrfs timeshift-autosnap inotify-tools cloudflare-warp-bin firefox betterbird-bin telegram-desktop gnome-disk-utility fastfetch vlc vlc-plugins-all mission-center komikku papirus-icon-theme papirus-folders bat wget 7zip unrar jre-openjdk qemu-full qemu-img libvirt virt-install virt-manager edk2-ovmf dnsmasq swtpm guestfs-tools libosinfo tuned"
+PACMANs="alacritty timeshift grub-btrfs timeshift-autosnap inotify-tools cloudflare-warp-bin firefox betterbird-bin telegram-desktop transmission-gtk gnome-disk-utility fastfetch vlc vlc-plugins-all mission-center komikku papirus-icon-theme papirus-folders extra/breeze bat wget 7zip unrar jre-openjdk qemu-full qemu-img libvirt virt-install virt-manager edk2-ovmf dnsmasq swtpm guestfs-tools libosinfo tuned"
 yay -S $PACMANs
-Gaming="alsa-lib alsa-plugins fontconfig gamemode gamescope giflib glfw gnutls gst-plugin-pipewire gst-plugin-va gst-plugins-bad gst-plugins-bad-libs gst-plugins-base gst-plugins-base-libs gst-plugins-good gst-plugins-ugly gtk2 gtk2+extra gtk3 libgcrypt libgpg-error libjpeg-turbo libldap libpng libpulse libva libva-mesa-driver libxcomposite libxinerama libxslt mangohud mangojuice mpg123 ncurses ocl-icd openal opencl-icd-loader sqlite adwsteamgtk steam proton-ge-custom-bin steamtinkerlaunch v4l-utils vkd3d vulkan-icd-loader prismlauncher dxvk-bin bottles wine-gecko wine-mono wine-staging winetricks lib32-alsa-lib lib32-alsa-plugins lib32-fontconfig lib32-giflib lib32-gnutls lib32-gst-plugins-base-libs lib32-gst-plugins-good lib32-gtk3 lib32-libgcrypt lib32-libgpg-error lib32-libjpeg-turbo lib32-libldap lib32-libpng lib32-libpulse lib32-libva lib32-libva-mesa-driver lib32-libxcomposite lib32 libxinerama lib32-libxslt lib32-mangohud lib32-mpg123 lib32-ncurses lib32-ocl-icd lib32-openal lib32-sqlite lib32-v4l-utils lib32-vkd3d lib32-vulkan-icd-loader"
+Gaming="alsa-lib alsa-plugins fontconfig gamemode gamescope giflib glfw gnutls gst-plugin-pipewire gst-plugin-va gst-plugins-bad gst-plugins-bad-libs gst-plugins-base gst-plugins-base-libs gst-plugins-good gst-plugins-ugly gtk2 gtk2+extra gtk3 libgcrypt libgpg-error libjpeg-turbo libldap libpng libpulse libva libva-mesa-driver libxcomposite libxinerama libxslt mangohud mangojuice mpg123 ncurses ocl-icd openal opencl-icd-loader sqlite adwsteamgtk steam proton-ge-custom-bin steamtinkerlaunch v4l-utils vkd3d vulkan-icd-loader prismlauncher dxvk-bin bottles wine-gecko wine-mono wine winetricks lib32-alsa-lib lib32-alsa-plugins lib32-fontconfig lib32-giflib lib32-gnutls lib32-gst-plugins-base-libs lib32-gst-plugins-good lib32-gtk3 lib32-libgcrypt lib32-libgpg-error lib32-libjpeg-turbo lib32-libldap lib32-libpng lib32-libpulse lib32-libva lib32-libva-mesa-driver lib32-libxcomposite lib32 libxinerama lib32-libxslt lib32-mangohud lib32-mpg123 lib32-ncurses lib32-ocl-icd lib32-openal lib32-sqlite lib32-v4l-utils lib32-vkd3d lib32-vulkan-icd-loader"
 yay -S $Gaming
 sudo sed -i s/#unix_sock_group = "libvirt"/unix_sock_group = "libvirt"/g /etc/libvirt/libvirtd.conf
 sudo sed -i s/#unix_sock_rw_perms = "0770"/unix_sock_rw_perms = "0770"/g /etc/libvirt/libvirtd.conf
@@ -52,7 +57,8 @@ yay -S $Fonts
 cd
 git clone https://github.com/vinceliuice/Orchis-theme.git
 cd Orchis-theme
-./install.sh --theme green --color dark --size standard --icon arch --libadwaita --tweaks solid compact dock
+# ./install.sh --theme green --color dark --size standard --icon arch --libadwaita --tweaks solid compact dock
+# ./install.sh --theme green --color dark --size standard --icon fedora --libadwaita --tweaks solid compact --round 0px
 sudo cp -r ~/.themes/* /usr/share/themes
 
 # My configs
