3dc47d9ae0e8e33be40100ce4a22c37fd41d55c0
LBarrys <201232108+LBarrys@users.noreply.github.com>
2025-09-02 17:37:57 +0300

    Update enrollkey.sh

--- fedora/enrollkey.sh
+++ fedora/enrollkey.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-sudo dnf install kmodtool akmods mokutil openssl
+sudo dnf install kmodtool akmods mokutil openssl -y
 sudo kmodgenca -a
 sudo mokutil --import /etc/pki/akmods/certs/public_key.der
