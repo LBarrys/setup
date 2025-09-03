490ce4b3f4db6da1e4296c453c6a3ece47b7f1f3
LBarrys <201232108+LBarrys@users.noreply.github.com>
2025-09-03 22:04:56 +0300

    Delete fedora/mirrors.sh

--- fedora/mirrors.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-echo "baseurl=https://mirror.maeen.sa/fedora/releases/42/Everything/x86_64/os/" | sudo tee -a /etc/yum.repos.d/fedora.repo
-echo "baseurl=https://mirror.maeen.sa/fedora/updates/42/Everything/x86_64/" | sudo tee -a /etc/yum.repos.d/fedora-updates.repo
